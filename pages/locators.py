c7ebf610d463771d459ab34af7275ba36e558c90
ushc-code <ir.ushakova000@gmail.com>
2023-12-28 16:15:01 +0400

    add login page assert

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    LOGIN_USERNAME = (By.ID, "id_login-username")
+    LOGIN_PASSWORD = (By.ID,"id_login-password")
+    LOGIN_BUTTON = (By.NAME,"login_submit")
+
+    REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_USERNAME = (By.ID, "id_registration-email")
+    REGISTER_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTER_PASSWORD_REPEAT = (By.ID, "id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
