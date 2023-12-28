270f8ae9437e23e8277234932242f8050820824b
ushc-code <ir.ushakova000@gmail.com>
2023-12-28 16:18:42 +0400

    init login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
 import pytest
 from selenium import webdriver
@@ -9,10 +10,12 @@ import time
 import math
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()# выполняем метод страницы — переходим на страницу логина
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
