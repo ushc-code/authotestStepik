91950985a58ca7f9b130d985fa9d2130f5a4da42
ushc-code <ir.ushakova000@gmail.com>
2023-12-28 15:30:17 +0400

    add pom structure

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 import pytest
 from selenium import webdriver
 from selenium.common import NoSuchElementException, StaleElementReferenceException, exceptions
@@ -9,6 +10,6 @@ import math
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
